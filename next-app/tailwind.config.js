fe8be8029e841f9e25206886f816837dc74408ff
Hattori-Takuma <hattori.ta@icloud.com>
2023-01-14 15:38:07 +0900

    next

---
+++ next-app/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
