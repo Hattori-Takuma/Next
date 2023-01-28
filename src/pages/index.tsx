bf3fd5ef541b47e06a3484a5e7010df892266df5
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-01-28 14:30:21 +0900

    Layoutコンポーネントを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
+import Layout from '@/components/Layout'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <>
+    <Layout>
       <section className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify-center flex-col">
       <h1 className="mb-4 text-green-500 text-3xl">サンプル</h1>
         <p className="mb-2 text-center">sample text</p>
@@ -11,6 +12,6 @@ export default function Home() {
       </Link>
       <button className="btn-blue">Let's Start!!</button>
       </section>
-    </>
+    </Layout>
   )
 }
